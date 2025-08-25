6af4b10bb12467bc498860b603659fd50a9e1499
Magnus <nharnharyhaw984@gmail.com>
2025-08-25 15:02:51 +0000

    chore: remove logging of request headers in streamable server

--- src/services/streamable.ts
+++ src/services/streamable.ts
@@ -37,9 +37,7 @@ export const startHTTPStreamableServer = async (
       res.writeHead(400).end("No URL");
       return;
     }
-    Logger.info(
-      `[Streamable] ${req.method} ${req.url} headers: ${JSON.stringify(req.headers)}`
-    );
+
     const reqUrl = new URL(req.url, "http://localhost");
 
     // Handle POST requests to endpoint
